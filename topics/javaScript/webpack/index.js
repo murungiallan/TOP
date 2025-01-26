0d2057531ea24d8df12035150aaccbf30ef1953f
Allan <allancheboiw0@gmail.com>
2025-01-27 02:33:21 +0300

    webpack

---
+++ topics/javaScript/webpack/index.js
@@ -0,0 +1,22 @@
+/**
+ * Lesson Overview
+ * -------------------
+ * Explain the purpose of bundlers and how they work
+ * Configure Webpack to bundle JavaScript modules
+ * Configure Webpack to handle non-JavaScript files during bundling, including using HtmlWebpackPlugin
+ * Setup Webpack's development server
+ **/
+
+/**
+ * --save-dev: It tells npm to record packages as development dependencies
+ */
+
+/**
+ * src and dist
+ * -------------
+ * When dealing with Webpack, there are 2 very important directories: 'src' (source) and 'dist' (distribution)
+ * src is where we keep all of our website's source code, essentially where all of our work will be done (with an exception being altering any configuration files in the root of the project)
+ * When we run Webpack to bundle our code, it will output the bundled files into the dist directory
+ * 
+ * Work inside 'src', build into 'dist', then deploy!
+ */
