7782d46a693fa39c88eae329ef98656918d513c0
Allan <allancheboiw0@gmail.com>
2025-01-27 02:00:08 +0300

    npm

---
+++ topics/javaScript/npm/index.js
@@ -0,0 +1,15 @@
+/**
+ * Lesson Overview
+ * -----------------
+ * Understand what npm is
+ * Understand the purpose of the package.json file
+ * 
+ * npm
+ * ====
+ * npm (Does not stand for 'Node Package Manager'!!) is a package manager
+ * It is a gigantic repository of plugins, libraries and other tools, which provides us with a command-line tool we can use to install these tools('packages') in our applications. We will then have all our installed packages' code locally, which we can import into our own files.
+ * 
+ * package.json
+ * =============
+ * npm revolves around this file
+ */
